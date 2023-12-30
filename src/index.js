d3d7c4cdacc3de2e23bf0c0a64909ceb18348147
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-12-31 07:04:52 +0800

    Memanfaatkan Props Untuk Component Lebih Dinamis

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating max={5} color="#fcc419" />
+    <StarRating max={10} color="blue" />
   </React.StrictMode>
 );
